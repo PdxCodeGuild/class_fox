85bf81cb8e53d9b2a57fc941ba1656f03601c7a6
adamthompson0 <mrthompson11@outlook.com>
2023-03-14 18:29:20 -0700

    Edited dictionary, elif statetments, and total.

--- code/adam/python/solutions/lab2.py
+++ code/adam/python/solutions/lab2.py
@@ -5,34 +5,34 @@ Mar 13, 2023
 """
 
 # Dictionary of blackjack card values
-blackjack = {"A": 1 or 11,
+blackjack = {"A": 1,
 "J": 10,
 "Q": 10,
 "K": 10,
-2: 2,
-3: 3,
-4: 4,
-5: 5,
-6: 6,
-7: 7,
-8: 8,
-9: 9,
-10: 10
+"2": 2,
+"3": 3,
+"4": 4,
+"5": 5,
+"6": 6,
+"7": 7,
+"8": 8,
+"9": 9,
+"10": 10
 }
 
 # User input of blackjack
-first_card = int(input("What's your first card: "))
-second_card = int(input("What's your second card: "))
-third_card = int(input("What's your third card: "))
+first_card = (input("What's your first card: "))
+second_card = (input("What's your second card: "))
+third_card = (input("What's your third card: "))
 
 # Total amount of cards adding up all 3 card values
-total = first_card + second_card + third_card
+total = blackjack[first_card] + blackjack[second_card] + blackjack[third_card]
 # if statement giving the user advice(Hit, Stay, Blackjack)
 if total == 21:
     print("21 Blackjack!")
 elif total < 15:
     print(f"{total} Hit")
-elif total >= 17 or total < 21:
+elif total > 17 and total < 21:
     print(f"{total} Stay")
-elif total > 21:
+else:
     print("Over 21, already busted!")
