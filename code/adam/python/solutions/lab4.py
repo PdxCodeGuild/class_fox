0b8fefcfd5670cf759b2a9f60e41d61a4cd3f727
adamthompson0 <mrthompson11@outlook.com>
2023-03-17 13:10:01 -0700

    Started Lab 4.

---
+++ code/adam/python/solutions/lab4.py
@@ -0,0 +1,25 @@
+"""
+Date: Mar 16th, 2023
+Lab 4: Credit Card Validation
+"""
+
+
+# 1.Convert the input string into a list of ints
+def credit_card_validator(card_number):
+    pass
+    
+
+ # 2.Slice off the last digit. That is the check digit.
+
+
+# 3.Reverse the digits.
+    
+# 4.Double every other element in the reversed list (starting with the first number in the list).
+    
+# 5.Subtract nine from numbers over nine.
+
+# 6.Sum all values.
+
+# 7.Take the second digit of that sum.
+
+# 8.If that matches the check digit, the whole card number is valid.
