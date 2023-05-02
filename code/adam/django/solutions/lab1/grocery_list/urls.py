4206a7e89284363163a57524feefbcb0f5952ed3
adamthompson0 <mrthompson11@outlook.com>
2023-05-01 20:16:22 -0700

    Finished lab 1

---
+++ code/adam/django/solutions/lab1/grocery_list/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add_item/', views.add_item, name='add_item'),
+    path('mark_purchased/<int:item_id>/', views.mark_purchased, name='mark_purchased'),
+]
