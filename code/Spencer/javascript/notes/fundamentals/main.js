0f2e73be9cda7c19bae8c89c1e1b3539ef6891e3
ssmith362 <spencey7@gmail.com>
2023-05-25 21:11:02 -0700

    javascript lab1

---
+++ code/Spencer/javascript/notes/fundamentals/main.js
@@ -0,0 +1,10 @@
+alert("Hello World")
+console.log("Hello from the console!")
+// console.error("hello")
+// console.warn("hi")
+// console.table("Beep")
+// console.info(["Bop"])
+
+// equivalent to input
+prompt("Enter your favorite color: ")
+
